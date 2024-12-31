995b2b35ca5507d0e25c21561cf16443dfc76ae4
rgorowsky <robert.gorowsky@sportngin.com>
2024-12-30 23:58:57 -0600

    worried this will fail again as well but going to commit lots of stuff in node modules it would seem

---
+++ lambda-deploy/server.ts
@@ -0,0 +1,39 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "us-east-2" });
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const { Name, Email, Subject, Comment } = JSON.parse(event.body || "{}");
+
+    const params = {
+      Source: "contact@betsybissonetterealty.com",
+      Destination: {
+        ToAddresses: ["rgorowsky@gmail.com"],
+      },
+      Message: {
+        Subject: { Data: Subject },
+        Body: { Text: { Data: Comment } },
+      },
+    };
+
+    await ses.sendEmail(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully!" }),
+    };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Failed to send email.",
+        error,
+      }),
+    };
+  }
+};
